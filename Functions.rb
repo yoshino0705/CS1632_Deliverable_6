a93ab9899fafb80d801b36debe11fefb4c584f54
nicholassallinger <nick.sallinger@gmail.com>
2018-04-06 12:23:26 -0400

    more functionality

--- Functions.rb
+++ Functions.rb
@@ -1,8 +1,6 @@
 class Functions
 
-# stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
-# => {"name"=>"Zed", "age"=>39, "height"=>74}
-# >> puts stuff['name']
+#TODO: Error codes
 
 @variables
 @text_file_array
@@ -26,7 +24,7 @@ class Functions
 
 	def parse_text_file(filename)
 		#parse a command line argument file
-		puts filename
+		#puts filename
 		File.open(filename).each do |line|
 			@text_file_array.push(line)
 			compute_line(line)
@@ -34,12 +32,28 @@ class Functions
 	end
 
 	def compute_line(line)
-		puts "> " + line
+		#puts "> " + line
 		split_line = line.split
+		if split_line.include?('LET') && split_line.index('LET') > 0
+			puts "error in command"
+			#quit if reading in file
+		elsif split_line.include?('LET') && split_line.index('LET') > 0
+			puts "error in command"
+			#quit if reading in file
+		elsif split_line.include?('LET') && split_line.index('LET') > 0
+			puts "error in command"
+			#quit if reading in file
+		end
+				
 		split_line.each do |command|
 			if command.eql? "LET"
 				let_index = line.index('LET')
-				assign(split_line[let_index+1],split_line[let_index+2])
+				if(let_index+3).nil?
+					assign(split_line[let_index+1],split_line[let_index+2])
+				else
+					assign(split_line[let_index+1],split_line[let_index+2],split_line[let_index+3],split_line[let_index+4])
+				end
+
 			elsif command.eql? "PRINT"
 				print_index = line.index('PRINT')
 				if !["+","-","/","*"].include?(split_line[print_index+3])
@@ -48,20 +62,41 @@ class Functions
 					print_command(split_line[print_index+1], split_line[print_index+2], split_line[print_index+3])
 				end
 			elsif command.eql? "QUIT"
-				quit_index = line.index('QUIT')
+				quit_command
 			else
+				#ERRORS FOR UNKNOWN KEYWORKDS
 			end
 		end
 	end
 
-	def end?
-		#line contains QUIT or end of file
+	def quit_command
+		exit
 	end
 
-	def assign(first,second)
-		#puts 'assigning ' + first.to_s + " to " + second.to_s
-		@variables[first] = second
-		puts second
+	def assign(first,second,third,operator)
+		if operator.nil?
+			@variables[first] = second
+		else
+			if @variables.include?(second)
+				second = @variables[second]
+			end
+			if @variables.include?(third)
+				third = @variables[third]
+			end
+
+			if operator.eql?("+")
+				@variables[first] = second.to_i + third.to_i
+			elsif operator.eql?("-")
+				@variables[first] = second.to_i + third.to_i
+			elsif operator.eql?("*")
+				@variables[first] = second.to_i * third.to_i
+			elsif operator.eql?("/")
+				@variables[first] = second.to_i / third.to_i
+			end
+		end
+
+		puts @variables
+			
 	end
 
 	def print_command(first,second,operator)
@@ -77,17 +112,8 @@ class Functions
 			second_value = second.to_i
 		end
 
-		#puts operator
-
-		# if first.is_a?(Integer)
-		# 	puts 'integer'
-		# else
-		# 	puts 'not an integer'
-		# end
-
-
 		if operator.nil?
-			puts first
+			puts first_value
 		elsif operator.eql? "+"
 			output = first_value + second_value
 			puts output
@@ -101,7 +127,6 @@ class Functions
 			output = first_value / second_value
 			puts output
 		end
-
 	end
 
 end
--- rpn.rb
+++ rpn.rb
@@ -1,16 +1,16 @@
 require_relative 'functions'
 # Execution starts here
 
-# stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
-# => {"name"=>"Zed", "age"=>39, "height"=>74}
-# >> puts stuff['name']
-
+# TODO: concatenate files if more than one command line argument
+# 		REPL mode
 functions = Functions.new()
 if ARGV.none?
 	# parse user input from command line
+	while(1)
+		print "> "
+		functions.compute_line(gets)
 	end
 else
-	command_line = true
 	functions.parse_text_file(ARGV[0])
 	# parse_text_file
 end
