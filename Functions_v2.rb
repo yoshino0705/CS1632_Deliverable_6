a6d75fa7e5bcc1f3e08cee59a7e27537191f06b2
Yoshino <weihaomouse@gmail.com>
2018-04-07 23:25:14 -0400

    added error codes

--- Functions_v2.rb
+++ Functions_v2.rb
@@ -1,23 +1,23 @@
 class RPN
-  attr_accessor :operands, :variables, :line_num, :result
+  attr_accessor :mode, :variables, :line_num, :result
   def initialize
-    @keywords = ['LET', 'PRINT', 'QUIT']
+  	@mode = 'REPL'
     @operands = []
     @result = 0
     @variables = {}
-    @line_num = 0
+    @line_num = 1
   end
 
   def calculate expression
     tokens = expression.split
     if tokens.length < 1
       puts "Line #{@line_num}: Expression too short"
-      return
+      return 5
     end
     if (string? tokens[0]) && (not alphabetical? tokens[0])
-      execute_keywords(tokens.drop(1), tokens[0].upcase)
+      return execute_keywords(tokens.drop(1), tokens[0].upcase)
     else
-      evaluate tokens
+      return evaluate tokens
     end
   end
 
@@ -34,7 +34,7 @@ class RPN
               @operands.push(@variables[t.upcase])
             else
           	  puts "Line #{@line_num}: Variable #{t} is not initialized"
-          	  return
+          	  return 1
             end
 
           elsif @operands.length >= 2
@@ -43,11 +43,11 @@ class RPN
           	  @operands.push(operands[0].send(t.to_s, operands[1]))
             rescue
           	  puts "Line #{@line_num}: Could not evaluate expression"
-          	  return
+          	  return 5
             end
           else
             puts "Line #{@line_num}: Operator #{t} applied to empty stack"
-            return
+            return 2
           end
       end
     end
@@ -55,9 +55,11 @@ class RPN
     if @operands.length == 1
       @result = @operands[0]      
       puts @result
+      return 0
     else
       puts "Line #{@line_num}: #{@operands.length} elements in stack after evaluation"
       @result = nil
+      return 3
     end
   end
 
@@ -66,25 +68,35 @@ class RPN
       when 'LET'
         let(tokens)
       when 'PRINT'
-        evaluate tokens
-        #puts @result
+      	if @mode == 'REPL'
+        	evaluate tokens
+        else
+        	evaluate tokens
+        	puts @result
+        end        
       when 'QUIT'
         exit
       else
       	puts "Line #{@line_num}: Unknown keyword #{keyword}"
+      	return 4
     end
   end
 
   def let(tokens)
     if tokens.length < 1
       puts "Line #{@line_num}: Variable name missing"
+      return 5
     elsif tokens.length == 1
       puts "Line #{@line_num}: Value missing"
+      return 5
     elsif not alphabetical? tokens[0].upcase
       puts "Line #{@line_num}: Invalid variable name"
+      return 5
     else
       evaluate tokens.drop(1)
-      @variables[tokens[0].upcase] = @result
+      if not @result.nil?
+        @variables[tokens[0].upcase] = @result
+      end
     end
 
   end
